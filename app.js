97bbc98727873572b3b44f41553e1691df410222
Prashik <gamreprashik03@gmail.com>
2023-01-05 13:10:58 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 //importing routes:
 const authRoutes = require('./routes/authRoutes');
 
@@ -8,7 +9,8 @@ const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -29,4 +31,18 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.get('/set-cookie', (req,res) => {
+  res.cookie('employee',true,{ maxAge : 1000 * 60 * 60 * 24 , secure : true});
+  res.send('you got the cookie!');
+});
+
+app.get('/read-cookies',(req,res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+})
+
 app.use(authRoutes);
+
+
